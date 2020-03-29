6cbce9d2fed676410d48e63b37348473e51951b7
ninox92 <thomasreynders@hotmail.nl>
2020-03-29 20:13:00 +0200

    fix(Socket.io): Change initialization order of express and socket.io
    - await nuxt.ready() before server.listen()

--- server/index.ts
+++ server/index.ts
@@ -2,46 +2,66 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import express from "express";
-import io from "./io";
+// import io from "./io";
+import SocketIO from "socket.io";
 import consola from "consola";
+import { Nuxt, Builder } from "nuxt";
 import { createServer, Server } from "http";
+import config from "../nuxt.config";
 
 dotenv.config();
 
-const port = 3000;
-const host = "0.0.0.0";
-
 const app = express();
 const server: Server = createServer(app);
+const io: SocketIO.Server = SocketIO(server);
 
-function listenAsync(): Promise<void> {
-  return new Promise((resolve, reject) => {
-    server
-      .listen(port, host)
-      .on("error", error => {
-        consola.error(error);
-        reject(error);
-      })
-      .on("listening", () => {
-        consola.info("Server listening");
-        resolve();
-      });
-  });
-}
+const host: string = "0.0.0.0";
+const port = process.env.PORT || 3000;
+const isProd: boolean = process.env.NODE_ENV === "production";
+config.dev = !isProd;
 
-// Load api middleware
-app.use(bodyParser.json());
-app.use(helmet());
-
-// Load api routes
 import routes from "./api";
 app.use("/api", routes);
 
-// Load Socket.IO
+// We instantiate Nuxt.js with the options
 (async () => {
-  io(host, port, server);
-  await listenAsync();
-})();
+  const nuxt = new Nuxt(config);
+  await nuxt.ready();
 
-// Export the server middleware
-export default app;
+  // Start build process in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    builder.build();
+  }
+
+  // Load api middleware
+  app.use(nuxt.render);
+  app.use(bodyParser.json());
+  app.use(helmet());
+
+  // Load api routes
+  server
+    .listen(port)
+    .on("error", error => {
+      consola.error(error);
+    })
+    .on("listening", () => {
+      consola.info("Server listening");
+    });
+
+  console.log("starting ws on " + host + ":" + port, server.listening);
+  const namespace = io.of("/proximity");
+  namespace.on("connection", (socket: SocketIO.Socket) => {
+    consola.info("Socket.IO connected to /proximity");
+    socket.on("proximity", (message: string, cb: Function) => {
+      consola.info(message);
+      socket.emit("proximity", { test: true });
+
+      cb();
+    });
+
+    socket.on("disconnect", () => {
+      consola.info("client disconnected from /proximity");
+    });
+  });
+})();
