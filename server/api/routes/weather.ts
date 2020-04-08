46e4b8c508c8afb020d5773b68e24855d9e2cfed
ninox92 <thomasreynders@hotmail.nl>
2020-04-08 13:37:46 +0200

    fix(weather): Remove unused variable

--- server/api/routes/weather.ts
+++ server/api/routes/weather.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response } from "express";
 import { check, validationResult } from "express-validator";
 import {
   OpenWeatherMapApi,
