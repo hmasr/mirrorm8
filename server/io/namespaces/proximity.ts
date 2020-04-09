9a35d552330b346756f86d72384352aeacaf2b74
ninox92 <thomasreynders@hotmail.nl>
2020-04-09 21:12:07 +0200

    feat(Proximity): Add signal proximity to toggle display power with vcgencmd
    - Enable node-pi-proximity with Gpio signal
    - Add configurable kick timer for signal (SIGNAL_TIMEOUT)

--- server/io/namespaces/proximity.ts
+++ server/io/namespaces/proximity.ts
@@ -1,12 +1,11 @@
 import SocketIO from "socket.io";
-// import { SignalProximity } from "node-pi-proximity";
-// import { Gpio } from "onoff";
-// import { execSync } from "child_process";
+import { SignalProximity } from "node-pi-proximity";
+import { Gpio } from "onoff";
+import { execSync } from "child_process";
 import consola from "consola";
 
 export default (io: SocketIO.Server) => {
   const of = io.of("/proximity");
-  // const gpio4 = new Gpio(4, "in", "both");
 
   of.on("connection", (socket: SocketIO.Socket) => {
     consola.info("Socket.IO connected to /proximity");
@@ -16,39 +15,44 @@ export default (io: SocketIO.Server) => {
     });
   });
 
-  let state: boolean = false;
-  setInterval(() => {
-    state = !state;
-    of.emit("change", state);
-  }, 1000);
+  if (!Gpio.accessible) {
+    let state: boolean = false;
+    setInterval(() => {
+      state = !state;
+      consola.info("Emit change");
+      of.emit("change", state);
+    }, 1000);
+  } else {
+    const gpio4 = new Gpio(4, "in", "both");
 
-  // const signalProximity = new SignalProximity(
-  //   gpio4,
-  //   process.env.SIGNAL_TIMEOUT as number | undefined
-  // );
+    const signalProximity = new SignalProximity(
+      gpio4,
+      process.env.SIGNAL_TIMEOUT as number | undefined
+    );
 
-  // signalProximity
-  //   .on("begin", () => {
-  //     console.log("Turn display ON");
-  //     try {
-  //       execSync("vcgencmd display_power 1");
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   })
-  //   .on("change", () => {
-  //     of.emit("change");
-  //   })
-  //   .on("end", () => {
-  //     console.log("Turn display OFF");
-  //     try {
-  //       execSync("vcgencmd display_power 0");
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   });
+    signalProximity
+      .on("begin", () => {
+        console.log("Turn display ON");
+        try {
+          execSync("vcgencmd display_power 1");
+        } catch (error) {
+          console.error(error);
+        }
+      })
+      .on("change", value => {
+        of.emit("change", Boolean(value));
+      })
+      .on("end", () => {
+        console.log("Turn display OFF");
+        try {
+          execSync("vcgencmd display_power 0");
+        } catch (error) {
+          console.error(error);
+        }
+      });
 
-  // process.on("SIGINT", () => {
-  //   signalProximity.stop();
-  // });
+    process.on("SIGINT", () => {
+      signalProximity.stop();
+    });
+  }
 };
