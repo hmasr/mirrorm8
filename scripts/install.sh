330da644e15a882b697ed19126539a9084f552ea
Thomas Reynders <thomasreynders@hotmail.nl>
2020-07-10 22:06:49 +0200

    build(scripts): Fix paths for systemd service run script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,27 +5,34 @@ SERVICE_SCRIPT_PATH=/usr/bin/mirrorm8.sh
 SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 
+echo "Creating systemd service $SERVICE_PATH"
 /bin/cat << EOF > $SERVICE_PATH
 [Unit]
 Description=Mirrorm8 (Smart Mirror) Service.
+After=network.target
 
 [Service]
 Type=simple
 ExecStart=/bin/bash $SERVICE_SCRIPT_PATH
+Restart=on-failure
 
 [Install]
-WantedBy=network-online.target
+WantedBy=multi-user.target
 EOF
 
 chmod 644 $SERVICE_PATH
 
-/bin/cat << EOF > $SERVICE_SCRIPT_PATH
-MIRRORM8_SCRIPTS=$SCRIPT_PATH
-cd .. && $MIRRORM8_ROOT
+echo "Creating run script $SERVICE_SCRIPT_PATH"
 
+/bin/cat << EOF > $SERVICE_SCRIPT_PATH
+cd "${SCRIPT_PATH}"
+cd ..
 npm start
 EOF
 
 chmod +x $SERVICE_SCRIPT_PATH
 
+echo "Enabling systemd service"
 systemctl enable mirrorm8.service
+
+echo "Done..."
